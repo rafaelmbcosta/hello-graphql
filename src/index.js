cd7f891402dada717db085a16ea13319a23e8f23
Rafael Costa <rafaelmbcosta@gmail.com>
2020-11-17 23:05:57 -0300

    spliting up files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import { GraphQLServer } from 'graphql-yoga'
+import db from './db'
+import Query from './resolvers/Query'
+import Mutation from './resolvers/Mutation'
+import User from './resolvers/User'
+import Post from './resolvers/Post'
+import Comment from './resolvers/Comment'
 
-// type definition
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers: {
+        Query,
+        Mutation,
+        User,
+        Post,
+        Comment
+    },
+    context: {
+        db
+    }
+})
 
-// Resolvers
+server.start(() => {
+    console.log('The server is up!')
+})
