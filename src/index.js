412325236458e587041e5feda38e9d1ff5f9c32f
Rafael Costa <rafael.costa@pge.ce.gov.br>
2020-11-18 10:02:18 -0300

    enums and subscriptions

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import { GraphQLServer, PubSub } from 'graphql-yoga'
 import db from './db'
 import Query from './resolvers/Query'
 import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
 import User from './resolvers/User'
 import Post from './resolvers/Post'
 import Comment from './resolvers/Comment'
-import Subscription from './resolvers/Subscription'
 
 const pubsub = new PubSub()
 
@@ -14,10 +14,10 @@ const server = new GraphQLServer({
     resolvers: {
         Query,
         Mutation,
+        Subscription,
         User,
         Post,
-        Comment,
-        Subscription
+        Comment
     },
     context: {
         db,
